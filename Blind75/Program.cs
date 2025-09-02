a2d1963837685b67fdc04ba0371a463a07aa61a2
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-02 11:49:37 +0800

    Contains Duplicate

--- Blind75/Program.cs
+++ Blind75/Program.cs
@@ -1,5 +1,5 @@
-﻿//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-//Example 1:
+﻿//Title: Contains Duplicate
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 //Input: nums = [1, 2, 3, 1]
 //Output: true
 //Explanation:
@@ -16,4 +16,28 @@
 //1 <= nums.length <= 105
 //- 109 <= nums[i] <= 109
 
+int[] nums = new int[] { 1, 2, 3, 1 };
 
+Solution.ContainsDuplicate(nums);
+
+public static class Solution
+{
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> ints = new HashSet<int>();
+        foreach(int num in nums)
+        {
+            ints.Add(num);
+        }
+        return nums.Length != ints.Count;
+    }
+}
+
+//Top1
+//public class Solution
+//{
+//    public bool ContainsDuplicate(int[] nums)
+//    {
+//        return new HashSet<int>(nums).Count < nums.Length;
+//    }
+//}
