f97707cb586206d8707cef84f2b08b00173a8c4b
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-02 21:30:58 +0800

    Group Anagrams

---
+++ Blind75.Core/Arrays_and_Hashing/GroupAnagrams.cs
@@ -0,0 +1,36 @@
+﻿//https://leetcode.com/problems/group-anagrams/description/
+
+//Input: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+//Output: [["bat"],["nat", "tan"],["ate", "eat", "tea"]]
+
+using System.Diagnostics.Tracing;
+
+namespace Blind75.Core.Arrays_and_Hashing
+{
+    public static class GroupAnagrams
+    {
+        public static IList<IList<string>> Solution(string[] strs)
+        {
+            //edge cases
+            if (strs.Length == 0) return new List<IList<string>>();
+            if (strs.Length == 1) return new List<IList<string>> { new List<string> { strs[0] } };
+
+            Dictionary<string, List<string>> hashTable = new Dictionary<string, List<string>>();
+
+            foreach (var word in strs)
+            {
+                char[] chars = word.ToCharArray();
+                Array.Sort(chars);
+                string key = new string(chars);
+                if (!hashTable.ContainsKey(key))
+                {
+                    hashTable[key] = new List<string>();
+                }
+                hashTable[key].Add(word);
+            }
+
+            return hashTable.Values.ToList<IList<string>>();
+        }
+    }
+}
--- Blind75.Core/Program.cs
+++ Blind75.Core/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Blind75.Core.Arrays_and_Hashing;
+using System;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
         ValidAnagram.Solution("anagram1", "nagarams");
-        //TwoSum.Solution(new int[] { 2, 7, 11, 15 }, 9);
         TwoSum.Solution(new int[] { 3,2,4 }, 6);
+        GroupAnagrams.Solution(["eat", "tea", "tan", "ate", "nat", "bat"]);
     }
 }
