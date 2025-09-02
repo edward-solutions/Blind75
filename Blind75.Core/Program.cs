b2126cdb01095105702987957ed00f219e1fb9f6
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-02 22:23:21 +0800

    Valid Palindrome

--- Blind75.Core/Program.cs
+++ Blind75.Core/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Blind75.Core.Arrays_and_Hashing;
+using Blind75.Core.Two_Pointers;
 using System;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
-        ValidAnagram.Solution("anagram1", "nagarams");
-        TwoSum.Solution(new int[] { 3,2,4 }, 6);
-        GroupAnagrams.Solution(["eat", "tea", "tan", "ate", "nat", "bat"]);
+        //ValidAnagram.Solution("anagram1", "nagarams");
+        //TwoSum.Solution(new int[] { 3,2,4 }, 6);
+        //GroupAnagrams.Solution(["eat", "tea", "tan", "ate", "nat", "bat"]);
+        ValidPalindrome.Solution("A man, a plan, a canal: Panama");
+        ValidPalindrome.Solution("A man, a plan, a canal: Panam123a");
     }
 }
---
+++ Blind75.Core/Two_Pointers/ValidPalindrome.cs
@@ -0,0 +1,26 @@
+﻿// a s s a
+// 1 2 3 4
+
+// a s s t s s a
+// 1 2 3 4 5 6 7
+namespace Blind75.Core.Two_Pointers
+{
+    public static class ValidPalindrome
+    {
+        public static bool Solution(string s)
+        {
+            char[] filteredChars = new string(s.Where(char.IsLetterOrDigit).ToArray()).ToLower().ToCharArray();
+            int left = 0;
+            int right = filteredChars.Length - 1;
+
+            while (left < right)
+            {
+                if (filteredChars[left] != filteredChars[right]) return false;
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
