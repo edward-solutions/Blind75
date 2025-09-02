d7d15d7b8f284791715648195cf32529029a1ef0
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-02 13:17:18 +0800

    Setup project directory and tests

---
+++ Blind75.Tests/1.ContainsDuplicateTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blind75.Tests
+{
+    public class ContainsDuplicateTests
+    {
+        [Fact]
+        public void ReturnsTrue_WhenArrayHasDuplicates()
+        {
+            int[] nums = new int[] { 1, 2, 3, 1 };
+            Assert.True(Solution.ContainsDuplicate(nums));
+        }
+        [Fact]
+        public void ReturnsTrue_WhenArrayHasDuplicatesTop1()
+        {
+            int[] nums = new int[] { 1, 2, 3, 1 };
+            Assert.True(Solution.ContainsDuplicateTop1(nums));
+        }
+    }
+}
