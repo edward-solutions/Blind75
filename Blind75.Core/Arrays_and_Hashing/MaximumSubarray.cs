e9e0a274e7697b7588fa592118591cc678dbb26f
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-03 22:17:05 +0800

    Maximum Sub array Kadane

---
+++ Blind75.Core/Arrays_and_Hashing/MaximumSubarray.cs
@@ -0,0 +1,20 @@
+﻿//https://leetcode.com/problems/maximum-subarray/
+//[-2,1,-3,4,-1,2,1,-5,4] = 6
+namespace Blind75.Core.Arrays_and_Hashing
+{
+    public static class MaximumSubarray
+    {
+        public static int Solution(int[] nums)
+        {
+            int currentSum = nums[0];
+            int maxSum = nums[0];
+
+            for(int i = 1; i < nums.Length; i++)
+            {
+                currentSum = int.Max(nums[i], nums[i] + currentSum);
+                maxSum = int.Max(maxSum, currentSum);
+            }
+            return maxSum;
+        }
+    }
+}
--- Blind75.Core/Program.cs
+++ Blind75.Core/Program.cs
@@ -9,7 +9,9 @@ public static class Program
         //ValidAnagram.Solution("anagram1", "nagarams");
         //TwoSum.Solution(new int[] { 3,2,4 }, 6);
         //GroupAnagrams.Solution(["eat", "tea", "tan", "ate", "nat", "bat"]);
-        ValidPalindrome.Solution("A man, a plan, a canal: Panama");
-        ValidPalindrome.Solution("A man, a plan, a canal: Panam123a");
+        //ValidPalindrome.Solution("A man, a plan, a canal: Panama");
+        //ValidPalindrome.Solution("A man, a plan, a canal: Panam123a");
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        Console.WriteLine(MaximumSubarray.Solution(nums));
     }
 }
