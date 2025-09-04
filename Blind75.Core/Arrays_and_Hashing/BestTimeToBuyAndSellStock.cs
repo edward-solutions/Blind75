c7e49b810c204606498a82d812c16f513f6b41fc
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-05 00:53:54 +0800

    BestTimeToBuyAndSellStock

---
+++ Blind75.Core/Arrays_and_Hashing/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,18 @@
+﻿public static class BestTimeToBuyAndSellStock
+{
+    public static int Solution(int[] prices)
+    {
+        int lowestStock = prices[0];
+        int maxProfit = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (lowestStock >= prices[i])
+            {
+                lowestStock = prices[i];
+            }
+            if (maxProfit < prices[i] - lowestStock)
+                maxProfit = prices[i] - lowestStock;
+        }
+        return maxProfit;
+    }
+}
