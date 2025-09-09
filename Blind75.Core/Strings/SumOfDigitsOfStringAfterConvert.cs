4a63daa1802ec0289849dd5cf198202a2d7e39c1
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-09 21:48:34 +0800

    sum-of-digits-of-string-after-convert

---
+++ Blind75.Core/Strings/SumOfDigitsOfStringAfterConvert.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75.Core.Strings
+{
+    public static class GetLucky
+    {
+        public static int Solution(string s, int k)
+        {
+            //zbax = 26 2 1 24, k = 2
+
+            string stringToNumber = string.Empty;
+            int result = 0;
+
+            //convert to numbers
+            foreach (char c in s)
+            {
+                stringToNumber += (c - 'a' + 1).ToString();
+            }
+
+            char[] charArray = stringToNumber.ToCharArray();
+
+            //loop until k
+            for (int i = 0; i < k; i++)
+            {
+                result = 0;
+                //loop until string length
+                for (int j = 0; j < charArray.Length; j++)
+                {
+                    result += charArray[j] - '0';
+                }
+                charArray = result.ToString().ToCharArray();
+            }
+
+            return result;
+
+        }
+    }
+}
