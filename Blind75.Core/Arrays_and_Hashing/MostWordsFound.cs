139973efea1a847b4c86f5f190a71f7c132e8ab2
edward cabanatan <edward.cabanatan@gmail.com>
2025-09-13 12:10:37 +0800

    MostWordsFound

---
+++ Blind75.Core/Arrays_and_Hashing/MostWordsFound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75.Core.Arrays_and_Hashing
+{
+    public class MostWordsFound
+    {
+        public int Solution(string[] sentences)
+        {
+            //count each words in each sentence inside the sentences array
+            //return the count
+            int count = 0;
+
+            foreach (string sentence in sentences)
+            {
+                var test = sentence.Split(' ');
+                Console.WriteLine(test.Count());
+                count = Math.Max(count, test.Count());
+            }
+            return count;
+        }
+    }
+}
